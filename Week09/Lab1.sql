e82fa31f9708d37dce4223b61a3d864c680afa67
Sev <90162122+sev2021@users.noreply.github.com>
2021-12-03 11:53:52 +0000

    Tasks 1 and 2

--- Week09/Lab1.sql
+++ Week09/Lab1.sql
@@ -2,8 +2,16 @@
 -- Task One 
 -- Create a sub query that will retrieve the postcode from property for rent where the owner has the phone number 0141-943-1728 
 
+USE dreamhome;
+SELECT * FROM propertyforrent WHERE ownerNO = (SELECT ownerNO FROM privateowner WHERE telNo = '0141-943-1728');
+
 -- Task Two 
 -- Create a sub query that will retrieve the full name of the assistant from staff where the branch postcode is G11 9QX 
+
+USE dreamhome;
+SELECT CONCAT(fname, lname) AS 'Full name' FROM staff 
+  WHERE position='Assistant' AND branchNo = 
+  (SELECT branchNo FROM branch WHERE postcode='G11 9QX');
 
 -- Task Three 
 -- Create a view that will display the property number postcode and type from properties for rent. 
