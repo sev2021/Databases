17ec59ba629272e5f777e220f9e61267a1bbb7d4
Sev <90162122+sev2021@users.noreply.github.com>
2021-12-03 16:56:07 +0000

    Lab for Week09 DONE!

--- Week09/Lab1.sql
+++ Week09/Lab1.sql
@@ -9,7 +9,7 @@ SELECT * FROM propertyforrent WHERE ownerNO = (SELECT ownerNO FROM privateowner 
 -- Create a sub query that will retrieve the full name of the assistant from staff where the branch postcode is G11 9QX 
 
 USE dreamhome;
-SELECT CONCAT(fname, lname) AS 'Full name' FROM staff 
+SELECT CONCAT(fname, ' ', lname) AS 'Full name' FROM staff 
   WHERE position='Assistant' AND branchNo = 
   (SELECT branchNo FROM branch WHERE postcode='G11 9QX');
 
@@ -17,11 +17,21 @@ SELECT CONCAT(fname, lname) AS 'Full name' FROM staff
 -- Create a view that will display the property number postcode and type from properties for rent. 
 -- Query the view 3 times using different queries and screenshot the results 
 
+USE dreamhouse;
+SELECT propertyNo, postcode, type FROM propertyforrent;
+
 -- Task Four  
--- Create a view that will display all details except gender from the staff table where the branch all of the following 4, 7 and 2 
+-- Create a view that will display all details except gender from the staff table where the branch( all of the following 4, 7 and 2 
+
+USE dreamhouse;
+SELECT staffNo, fName, lName, position, DOB, salary, branchNo FROM staff WHERE branchNo IN ('B002','B004', 'B007');
 
 -- Task Five 
 -- Try and create a view based on a sub query. Use any two tables of your choice for this 
+
+-- Create a query that retrieve full name of worker who register first client
+USE dreamhouse;
+SELECT fName, lName from staff WHERE staffNo = (SELECT staffNo FROM registration ORDER BY dateJoined LIMIT 1);
 
 -- Task Six 
 -- Save and upload the lab to the provided Moodle link. The solution will be supplied to you in class to compare your answers to.
